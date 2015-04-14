c00aea1a0b677ca59ee51317aba48c4fdba1ceae
JJ Asghar <jj@getchef.com>
2015-04-14 10:31:23 -0500

    This is another test
    Change-Id: Icd07ab2f5f1b3b2b4669a2781561717fcf5d5e05

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+# THIS IS ANOTHER TEST
 ## This script is for installing all the needed packages on trusty to run the chef tests with 'chef exec rake'.
 ## It relies on the common bootstrap.sh from stackforge/cookbook-openstack-common for installing common dependencies.
 
